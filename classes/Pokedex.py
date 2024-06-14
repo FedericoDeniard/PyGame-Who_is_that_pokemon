1934ea58c55bcea0a645ff51210f86c02831ffa9
FedericoDeniard <fededeniard@gmail.com>
2024-06-14 13:36:28 -0300

    Se agregó getters de nombres

--- classes/Pokedex.py
+++ classes/Pokedex.py
@@ -1,2 +1,13 @@
+from Pokemon import Pokemon
+
 class Pokedex:
-    pass
+    def __init__(self, pokemons:list[Pokemon]):
+        self.pokemons = []
+        for pokemon in pokemons:
+            self.pokemons.append(Pokemon(pokemon['names'], pokemon['image_path'], pokemon['gen']))
+    
+    def get_names(self) -> list[str]:
+
+        names = []
+        for pokemon in self.pokemons:
+            names.append(Pokemon)
