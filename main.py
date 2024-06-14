d318c1ab8804593e39a2b015ac67ad7516c81dbe
JoaquinFerman <joacoferman@gmail.com>
2024-06-14 17:10:19 -0300

    Clases Pokemon y Pokedex creadas

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
-import pygame
+from classes.class_pokedex import Pokedex
+import json
+
+config = 'config.json'
+config_data = {}
+
+with open(config, 'r') as file:
+    config_data = json.load(file)
 
 
-pygame.init()
+pokedex = Pokedex(config_data['pokemons'])
 
-pygame.quit()
+# print(pokedex.check_name("pikachu"))
+# print(pokedex.check_name("Bulbasaur"))
+# print(pokedex.check_name("Bisasam"))
+# print(pokedex.check_name("Ivysaur"))
+
+easy_level = pokedex.get_difficulty('easy')
+print(pokedex.check_name('Ivysaur', easy_level))
