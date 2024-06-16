6f123963ade1a7b2d351c42e5d9162dade5092e1
FedericoDeniard <fededeniard@gmail.com>
2024-06-16 12:39:42 -0300

    Se creó un constructor de botones

--- components/button.py
+++ components/button.py
@@ -1,9 +1,9 @@
+from assets.colours.main import colours
 import pygame
 import sys
-from assets.colours.main import colours
 
 class Button:
-    def __init__(self, screen, colour, position: tuple, border_radius=0, text="", font='Calibri', font_size=40, border_colour = None, border_width = 0):
+    def __init__(self, screen, colour, position: tuple, border_radius=0, text="", font='Calibri', font_size=40, border_colour = None, border_width = 0, text_colour = (0,0,0)):
         self.screen = screen
         self.colour = colour
         self.position = position
@@ -13,22 +13,27 @@ class Button:
         self.font_size = font_size
         self.border_colour = border_colour
         self.border_width = border_width
+        self.text_colour = text_colour
 
     def draw_button(self):
         if self.border_colour and self.border_width > 0:
-            pygame.draw.rect(self.screen, self.border_colour, self.position, border_radius=self.border_radius)
+            self.hitbox = pygame.draw.rect(self.screen, self.border_colour, self.position, border_radius=self.border_radius)
             inner_position = (
                 self.position[0] + self.border_width,
                 self.position[1] + self.border_width,
                 self.position[2] - 2 * self.border_width,
                 self.position[3] - 2 * self.border_width
             )
-            pygame.draw.rect(self.screen, self.colour, inner_position, border_radius=self.border_radius)
+            border_radius = self.border_radius - self.border_width if self.border_radius > 0 and self.border_radius > self.border_width else 0
+            pygame.draw.rect(self.screen, self.colour, inner_position, border_radius=border_radius)
         else:
-            pygame.draw.rect(self.screen, self.colour, self.position, border_radius = self.border_radius + self.border_width)
+            self.hitbox = pygame.draw.rect(self.screen, self.colour, self.position, border_radius = self.border_radius)
 
         if self.text:
             myFont = pygame.font.SysFont(self.font, self.font_size)
-            text_surface = myFont.render(self.text, True, (0, 0, 0))  # Text, Antialiasing, colour
+            text_surface = myFont.render(self.text, True, self.text_colour)  # Text, Antialiasing, colour
             text_rect = text_surface.get_rect(center=(self.position[0] + self.position[2] // 2, self.position[1] + self.position[3] // 2))
             self.screen.blit(text_surface, text_rect)
+
+    def get_hitbox(self):
+        return self.hitbox
--- main.py
+++ main.py
@@ -32,8 +32,10 @@ window = pygame.display.set_mode(WINDOW)
 backround = pygame.image.load('assets/interface/backround.jpg')
 backround = pygame.transform.scale(backround, (WINDOW_WIDTH, WINDOW_HEIGHT))
 window.blit(backround, (0,0))
-button = Button(window, colours['WHITE'], (100, 100, 200, 50), border_radius=10, text="Jugar", font_size=30, border_colour=colours["BLACK"], border_width=2)
 
+button = Button(window, colours['WHITE'], (500, 500, 200, 50),  text="Salir", font_size=30, border_colour=colours["BLACK"], border_width=4, border_radius=3)
+button.draw_button()
+button_hitbox = button.get_hitbox()
 
 run_flag = True
 while run_flag == True:
@@ -41,8 +43,9 @@ while run_flag == True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run_flag = False
-        # if event.type == pygame.MOUSEBUTTONDOWN:
-        #     window.blit()
-    button.draw_button()
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if button_hitbox.collidepoint(event.pos):
+                run_flag = False
+                
 
     pygame.display.update()
