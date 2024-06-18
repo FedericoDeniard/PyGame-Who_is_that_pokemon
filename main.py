c4cb0ebc459f611e0522cd371adb38de683cf48c
FedericoDeniard <fededeniard@gmail.com>
2024-06-18 17:19:15 -0300

    Se agregaron canciones al juego

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from classes.class_pokedex import Pokedex
 import pygame
 import json
+from random import randint
 
 from assets.colours.colours import colours
 from components.buttons import Button, Textbox
@@ -14,6 +15,16 @@ pokemon_font = 'console'
 WINDOW_WIDTH = 1200
 WINDOW_HEIGHT = 800
 WINDOW = (WINDOW_WIDTH, WINDOW_HEIGHT)
+
+soundtrack = ["assets/music/Ending.mp3","assets/music/Gym.mp3","assets/music/Opening.mp3","assets/music/Pallet_Town.mp3","assets/music/Pokemon_Center.mp3"]
+
+
+def play_random_music(soundtrack):
+    if not pygame.mixer.music.get_busy():
+        pygame.mixer.init()
+        pygame.mixer.music.load(soundtrack[randint(0, len(soundtrack) - 1)])
+        pygame.mixer.music.set_volume(.2)
+        pygame.mixer.music.play()
 
 with open(config, 'r') as file:
     config_data = json.load(file)
@@ -56,6 +67,7 @@ game = False
 run_flag = True
 
 while run_flag == True:
+    play_random_music(soundtrack)
     #region Draw Main Menu
     if main_menu:
         window.blit(main_menu_backround, (0,0))
